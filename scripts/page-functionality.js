d24223aa3f8c396128ec9f6f901b400b8354f306
Rounak Karmakar <rounakk2011@gmail.com>
2025-02-19 22:59:46 +0530

    Completed frontend of root index.html and working on dark_mode/light_mode toggling

---
+++ scripts/page-functionality.js
@@ -0,0 +1,6 @@
+document.getElementById("goto-pg_solve").addEventListener('click', function() {
+    window.location.href = "/solve/index.html";
+})
+document.getElementById("goto-pg_generate").addEventListener('click', function() {
+    window.location.href = "/generate/index.html";
+})
