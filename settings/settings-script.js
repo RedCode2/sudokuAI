e41174c4d0eb1c9f766ea543584d91972215de7c
Rounak Karmakar <rounakk2011@gmail.com>
2025-02-21 23:06:00 +0530

    Customizable font-size

--- settings/settings-script.js
+++ settings/settings-script.js
@@ -4,6 +4,7 @@ var s_font = document.getElementById('font');
 var s_fontSize = document.getElementById('font-size');
 
 set_and_save_ff('bodyElement', localStorage.getItem('font-family'), 'font-family');
+set_and_save_fs('bodyElement', localStorage.getItem('font-size'), 'font-size');
 
 function set_and_save_ff(applyToElement, fontFamily, localStorageKey) {
     let fontTranslate = {
@@ -15,6 +16,19 @@ function set_and_save_ff(applyToElement, fontFamily, localStorageKey) {
     if (fontFamily in fontTranslate) {
         document.getElementById(applyToElement).style.fontFamily = fontTranslate[fontFamily];
         localStorage.setItem(localStorageKey, fontFamily);
+    }
+}
+
+function set_and_save_fs(applyToElement, fontSize, localStorageKey) {
+    let fontSizeTranslate = {
+        "small": "14px",
+        "medium": "16px",
+        "large": "18px"
+    }
+
+    if (fontSize in fontSizeTranslate) {
+        document.getElementById(applyToElement).style.fontSize = fontSizeTranslate[fontSize];
+        localStorage.setItem(localStorageKey, fontSize);
     }
 }
 
@@ -39,4 +53,7 @@ s_switchToDarkMode.addEventListener('click', function () {
 
 s_font.addEventListener('change', function() {
     set_and_save_ff('bodyElement', s_font.value, 'font-family')
+})
+s_fontSize.addEventListener('change', function() {
+    set_and_save_fs('bodyElement', s_fontSize.value, 'font-size')
 })
